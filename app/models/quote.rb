54baf1931b4753ebe2dc6825464f7eb3b66a6fab
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-10 14:02:45 -0400

    add topic_names helper to topic model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,8 @@ class Quote < ActiveRecord::Base
     belongs_to :author
     has_many :quote_topics
     has_many :topics, through: :quote_topics
+
+    def topic_names
+        topics.map(&:name)
+    end
 end
